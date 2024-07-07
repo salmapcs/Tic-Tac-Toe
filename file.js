2598aaf7178a67bb12c4a264a4d5f1a6b03f9d99
Salma <salmapcs2422@gmail.com>
2024-07-07 22:32:26 +0530

    commit

--- file.js
+++ file.js
@@ -6,8 +6,6 @@ let message = document.querySelector("#mesg")
 let turnO = true;
 const winners = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
 
-
-
 const resetGame =() =>{
     turnO = true;
     enableboxes()
@@ -16,7 +14,7 @@ const resetGame =() =>{
 }
 boxes.forEach((box)=>{
     box.addEventListener("click",()=>{
-      
+        console.log("button was clicked")
         if(turnO){
          box.innerText= ("X")
          turnO =false;
@@ -33,7 +31,7 @@ boxes.forEach((box)=>{
 
 const disableboxes =()=>{
     for(let box of boxes){
-        boxes.disabled=true;
+        box.disabled=true;
     }
 }
 
@@ -62,6 +60,7 @@ const chckwinner=()=>{
             console.log("winner",pos1val)
             
             showWinner(pos1val)
+
         } 
      }
     }
